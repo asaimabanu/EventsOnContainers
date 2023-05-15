2e2748c7091f674aea9011b5d062935b7dececf7
asaimabanu <124813515+asaimabanu@users.noreply.github.com>
2023-05-14 23:36:25 -0700

    Added EventsWebMvc and APIPaths class

---
+++ EventsWebMvc/Infrastructure/APIPaths.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventsWebMvc.Infrastructure
+{
+    public static class APIPaths
+    {
+        public static class EventCatalog
+        {
+            public static string GetAllEventItems(string baseUrl,int page, int take)
+            {
+               
+                return $"{baseUrl}/EventItems?pageIndex={page}&pageSize={take}";
+               
+
+            }
+            public static string GetAllEventCategories(string baseUri)
+            {
+                return $"{baseUri}/EventCategories";
+            }
+
+
+            public static string GetFilteredEventItems(string baseUri,
+               int? EventId, int? EventCategoryId, bool? IsOnline, int? city, int page, int take)
+            {   
+                var preUri = string.Empty;
+                var filterQS = string.Empty;
+                
+
+
+            }
+
+        }
+       
+    }
+}
