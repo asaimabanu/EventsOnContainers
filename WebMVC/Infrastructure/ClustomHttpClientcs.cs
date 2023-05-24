9e75da7cd26736872c1d86b6883b5dd412201f20
Ruckumane Krishnasamy Hariharan <rucks15@gmail.com>
2023-05-24 14:55:36 -0700

    Recent changes

---
+++ WebMVC/Infrastructure/ClustomHttpClientcs.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace WebMvc.Infrastructure
+{
+    public class CustomHttpClientcs : IHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomHttpClientcs()
+        {
+            _httpClient = new HttpClient();
+        }
+        async Task<string> IHttpClient.GetStringAsync(string url, 
+            string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            if (authorizationToken != null)
+                {
+                request.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod,authorizationToken);
+                }
+            var response = await _httpClient.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
